40a04865569af8269c76e8c2ae287f3b302f8466
NERO <mxzxro@gmail.com>
2024-05-19 17:04:46 +0900

    Initial Commit

---
+++ NeologismFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NeologismFinder
+//
+//  Created by NERO on 5/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
